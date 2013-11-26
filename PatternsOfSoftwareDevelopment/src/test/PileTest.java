b24747c52cba690e4b3693bf516a3a1533138494
KevinBlom <kevin.a.blom@gmail.com>
2013-11-26 13:02:03 +0100

    Made a branch for Kevin. Included a test for pile (unfinished)

---
+++ PatternsOfSoftwareDevelopment/src/test/PileTest.java
@@ -0,0 +1,19 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PileTest {
+
+	@Test
+	public void testPop() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPush() {
+		fail("Not yet implemented");
+	}
+
+}
