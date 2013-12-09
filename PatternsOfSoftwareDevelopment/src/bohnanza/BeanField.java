95732cb5b49458432ae8c8f6fd862a5cd4c9385a
KevinBlom <kevin.a.blom@gmail.com>
2013-12-09 13:21:21 +0100

    BeanField has been updated to succesfully implement getHarvestValue() method

--- PatternsOfSoftwareDevelopment/src/bohnanza/BeanField.java
+++ PatternsOfSoftwareDevelopment/src/bohnanza/BeanField.java
@@ -5,7 +5,21 @@ public class BeanField extends Pile {
 	private BeanType type;
 
 	public int getHarvestValue() {
-		return 0;
+		int harvestValue = 0;
+		int amountOfCards = listOfCards.size();
+		
+		if(amountOfCards > 0) // If there are cards
+		{
+			Card harvestCard = listOfCards.get(0);
+			for(int i = 0; i < harvestCard.getBeanometer().size(); i++){
+				if(amountOfCards < harvestCard.getBeanometer().get(i).getCardsNecessary() && i > 0){ // if harvested cards have any value
+					for(int u = 0; u < harvestCard.getBeanometer().get(i-1).getProfit(); u++){ // loop for profit
+						harvestValue++;
+					}
+				}
+			}
+		}
+		return harvestValue;
 	}
 
 	public BeanType getBeanType() {
