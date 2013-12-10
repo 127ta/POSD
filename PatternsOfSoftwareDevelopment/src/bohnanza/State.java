143bb9e138ce753be7b6c01d9d91f32db4888efb
KevinBlom <kevin.a.blom@gmail.com>
2013-12-10 14:43:18 +0100

    Added State class to implement states according to state pattern

--- PatternsOfSoftwareDevelopment/src/bohnanza/State.java
+++ PatternsOfSoftwareDevelopment/src/bohnanza/State.java
@@ -1,5 +1,57 @@
 package bohnanza;
+import interfaces.IState;
 
-public enum State {
-	PLANT, DRAWTRADEDONATE, PLANTTRADEDONATED, DRAWNEW, INACTIVE
-}
+public abstract class State implements IState 
+{
+	public class Plant extends State
+	{
+
+		@Override
+		public String getStateInstruction() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+		
+	}
+	public class DrawTradeDonate extends State
+	{
+
+		@Override
+		public String getStateInstruction() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+		
+	}
+	public class PlantTradedDonated extends State
+	{
+
+		@Override
+		public String getStateInstruction() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+		
+	}
+	public class DrawNew extends State
+	{
+
+		@Override
+		public String getStateInstruction() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+		
+	}
+	public class Inactive extends State
+	{
+
+		@Override
+		public String getStateInstruction() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+		
+	}
+};
+
